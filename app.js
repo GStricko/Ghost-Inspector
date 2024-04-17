51866ebac2949de4a3bf35506b2e67be29321832
George Strickland <george.stricko@gmail.com>
2024-04-17 12:45:55 +1200

    removed screenshot spacing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function updateOutput() {
             const stepsArray = jsonData.data[0].steps;
             const lastStepIndex = stepsArray.length - 1;
             const lastStep = stepsArray[lastStepIndex];
-            const screenshot = `**Screenshot** \n\n${jsonData.data[0].screenshot.original.defaultUrl}`;
+            const screenshot = `**Screenshot** \n${jsonData.data[0].screenshot.original.defaultUrl}`;
 
 
             if (lastStep && lastStep.extra && lastStep.extra.accessibility && lastStep.extra.accessibility.issues) {
